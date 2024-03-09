bf4bf1178f746c22dedf25d17dfcfef52d4067d5
Sambit Poddar <68020106+sambitpoddar@users.noreply.github.com>
2024-03-09 19:29:41 +0530

    Update __init__.py

--- ethorb/__init__.py
+++ ethorb/__init__.py
@@ -5,4 +5,9 @@ EthOrb is a Python package for interacting with Ethereum blockchain networks.
 It provides a convenient interface for sending transactions, deploying smart contracts, and more.
 """
 
-__version__ = "0.0.9"
+__version__ = "1.0.0"
+
+# Import submodules for ease of use
+from . import contracts
+from . import tests
+from . import docs
