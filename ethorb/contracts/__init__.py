082229315e6a8d4afd19c40c6105086d8c912148
Sambit Poddar <68020106+sambitpoddar@users.noreply.github.com>
2024-03-09 19:31:37 +0530

    Update __init__.py

--- ethorb/contracts/__init__.py
+++ ethorb/contracts/__init__.py
@@ -2,6 +2,7 @@
 EthOrb Contracts Module
 
 This module contains ABIs and bytecodes for smart contracts used in EthOrb.
+You are supposed to add your own files while considering operations.
 """
 
 # Import submodules for ease of use
